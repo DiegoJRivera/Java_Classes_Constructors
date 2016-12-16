594c31d30e7c941817c29296e92832e5a514c85d
DiegoJRivera <DiegoJRivera@users.noreply.github.com>
2016-12-16 11:09:10 +1100

    Update Address.java

--- Address.java
+++ Address.java
@@ -1,7 +1,6 @@
 /*
  * Practical Exercise 2 developed by Diego Rivera for Programming Fundamentals class.
  * 16/08/2016
- * Professor ------------
  * This class has fields for: suburb and post code only. This class should also
  * have 2 constructors. A default constructor with the default values of (Melbourne, 3000)
  * and a second constructor that gets values as parameters.
